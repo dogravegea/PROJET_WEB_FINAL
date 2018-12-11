3efc0b4f07742740b63cc96ce055f0b890f8c582
Dorian GRAVEGEAL <dorian.gravegeal@gmail.com>
2018-12-11 16:24:09 +0100

    Ajout urlPhoto property to monkey

--- src/models/monkey.js
+++ src/models/monkey.js
@@ -2,7 +2,8 @@ module.exports = (sequelize, DataTypes) => {
     var Monkey = sequelize.define('Monkey', {
         name: DataTypes.STRING,
         age: DataTypes.INTEGER,
-        espece: DataTypes.STRING
+        espece: DataTypes.STRING,
+        urlPhoto: DataTypes.STRING
     });
 
     return Monkey;
